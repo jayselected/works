818eb19e75abe497a3d8b5ddd9559e52bacacc8e
jayselected <80530515+jayselected@users.noreply.github.com>
2026-07-27 17:58:02 +0100

    Update siteapp.js

--- siteapp.js
+++ siteapp.js
@@ -79,7 +79,7 @@ const THEME_STORAGE_KEY = 'portfolio-theme';
    the page. */
 const THEME_BACKGROUNDS = {
     light: '#FCFBF9',
-    dark:  '#0D1114'
+    dark:  '#000000'
 };
 
 const THEME_ICONS = {
